4bd20e757f7dea4ac27ba5a2cf28a9b830887654
Ryan Hunt <r-g-hunt@hotmail.com>
2022-09-19 10:27:30 +1200

    DB setup

---
+++ server/db/migrations/20220918221413_movies.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('movies', (table) => {
+    table.increments('id')
+    table.string('title')
+    table.string('img')
+    table.string('imdb_id')
+    table.boolean('watched')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('movies')
+  
+};
---
+++ server/db/seeds/movies.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('movies').del()
+  await knex('movies').insert(
+    [
+      { 
+        id: 1,
+        title: 'The Batman',
+        imdb_id: 'tt1877830',
+        watched: false,
+        img: 'https://imdb-api.com/images/original/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_Ratio0.7273_AL_.jpg'
+      },
+      { 
+        id: 2,
+        title: 'Arrival',
+        imdb_id: 'tt2543164',
+        watched: false,
+        img: 'https://imdb-api.com/images/original/MV5BNGU0NTA2YjctYWNlYy00ZDg1LTg5ZTItZWM3MWZiMDI5OGYzL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDM3ODU2NDM@._V1_Ratio0.7273_AL_.jpg'
+      },
+    ]
+  );
+};
